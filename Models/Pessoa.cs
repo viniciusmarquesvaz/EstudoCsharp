2843922cd165688032a0a1162ec113b11dbcd1a7
viniciusmarquesvaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2021-12-05 19:50:51 -0300

    comit classes

---
+++ Models/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace ExemploPOO.Models
+{
+  public class Pessoa
+  {
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public void Apresentar()
+    {
+      Console.WriteLine("Olá meu nome é " + Nome + " com" + Idade + "idade");
+    }
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // valores validos
+      Retangulo r = new Retangulo();
+      r.DefinirMedidas(30, 30);
+      System.Console.WriteLine("área é:  " + r.ObterArea());
+
+
+      // valores invalidos
+      Retangulo r2 = new Retangulo();
+      r.DefinirMedidas(0, 0);
+      System.Console.WriteLine("área é:  " + r2.ObterArea());
+
+
+
+      // Pessoa p1 = new Pessoa();
+
+      // p1.Nome = "Bob";
+      // p1.Idade = 20;
+      // p1.Apresentar();
+    }
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+}
